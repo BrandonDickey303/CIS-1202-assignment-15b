8e3e1802fe5b00028f0118aff5fbc7ef073e05dd
Brandon Dickey <brandon.dickey@sinclair.edu>
2021-05-02 17:22:58 -0400

    Added template called M

--- Source.cpp
+++ Source.cpp
@@ -3,3 +3,23 @@
 //May 2, 2021
 
 #include<iostream>
+
+using namespace std;
+
+template <typename M>
+
+M half(T val) {
+
+	M res;
+
+	res = val / 2;
+
+	if (*typeid(val).name() == 'i') {
+		if (res * 2 != val)
+
+		{
+			res++;
+		}
+	}
+	return res;
+}
